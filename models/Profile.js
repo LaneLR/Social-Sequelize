23505e9a13cf15e5195997a834117e8073a2a67c
LaneLR <laneleerichardson@gmail.com>
2025-05-14 14:42:36 -0500

    created Comment, Like, Post, Profile, and User models and their associations

--- models/Profile.js
+++ models/Profile.js
@@ -1,5 +1,12 @@
+const { db } = require("../db/connection")
+const DataTypes = require("sequelize")
 
-let Profile;
+let Profile = db.define("Profile", {
+    bio: DataTypes.STRING,
+    profilePicture: DataTypes.STRING,
+    birthday: DataTypes.STRING,
+});
+
 
 
 module.exports = Profile;
